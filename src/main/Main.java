d121911bf62d0b20e75abf2d41837be001bf515a
JulienRouse <julien.rouse@gmail.com>
2015-10-09 16:56:56 +0200

    corrected the main

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,22 +12,11 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 
-	//	Exo exo = new Exo();
-		//int res = exo.f(4, 4, 1,1);
-		//System.out.println("res: "+res+" en "+exo.compteurAppel+" appels récursifs") ;
-		
-		Double[] arrayName = new Double[4];
+		Exo exo = new Exo();
+		int res = exo.f(4, 4, 1,1);
+		System.out.println("res: "+res+" en "+exo.compteurAppel+" appels récursifs") ;
 		
 		
-		HashMap<Integer,ArrayList<Double>> hashmap = new HashMap<Integer, ArrayList<Double>>(); 
-		ArrayList<Double> l = new ArrayList<Double>();
-		l.add(2.);
-		l.add(3.);
-		
-		hashmap.put(1, l);
-		
-		arrayName = hashmap.get(1).toArray(arrayName);
-		System.out.println(arrayName[0]);
 	}
 
 }
