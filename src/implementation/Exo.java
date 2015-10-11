ee76974dd1284af46177d5730fa07980e0b21ff8
Julien Rouse <julien.rouse@gmail.com>
2015-10-12 01:09:47 +0200

    dp_symmetry

--- src/implementation/Exo.java
+++ src/implementation/Exo.java
@@ -1,13 +1,22 @@
 package implementation;
 
+import util.Simulate4D;
+import util.Symmetry;
+
 public class Exo {
 
 	
 public int compteurAppel;
 
+	int[] tabRes;
+	boolean[] tabChecked;
+	int m_initial;
+	int n_initial;
+
 	public Exo()
 	{
 		this.compteurAppel=0;
+		this.tabRes = null;
 	}
 		
 	
@@ -18,7 +27,7 @@ public int compteurAppel;
 			this.compteurAppel=0;
 		this.compteurAppel++;
 		
-		
+		//System.out.println(m+" "+n+" "+i+" "+j);
 		
 		if(m==1 && n==1)
 			return 0;
@@ -107,12 +116,223 @@ public int compteurAppel;
 	
 	public int f_dp_naif(int m,int n,int i,int j ,boolean resetCounter)
 	{
-		return 0;
+		if(resetCounter)
+			this.compteurAppel=0;
+		this.compteurAppel++;
+		
+		System.out.println(m+" "+n+" "+i+" "+j);
+		
+		if(m==1 && n==1)
+			return 0;
+		
+		int tmpI,tmpJ,res,tmpM,tmpN = -1;
+		int maxNegativ = Integer.MIN_VALUE;
+		boolean hasNeg = false;
+		int maxPositiv = 0;
+		
+		res = 0;
+		for(int indice = 1; indice < m; indice++ )
+		{		
+			tmpI = i;
+			tmpJ = j;
+			tmpM = indice;
+			
+			if(i>= indice)
+			{
+				tmpI = i - indice;
+				tmpM = m-indice;
+			}
+			
+			//System.out.println(tmpM+" "+n+" "+tmpI+" "+tmpJ);
+			if(tabRes[Simulate4D.convert(tmpM, n, tmpI, tmpJ, m_initial, n_initial, m_initial)]==0)
+			{
+				res = f_dp_naif(tmpM, n, tmpI, tmpJ,false);
+				tabRes[Simulate4D.convert(tmpM, n, tmpI, tmpJ, m_initial, n_initial, m_initial)]=res;
+			}
+			else
+				res = tabRes[Simulate4D.convert(tmpM, n, tmpI, tmpJ, m_initial, n_initial, m_initial)];
+			
+			if (res<1)
+			{
+				hasNeg = true;
+				maxNegativ = Math.max(maxNegativ, res);
+			}
+			else
+				maxPositiv = Math.max(maxNegativ, res);
+			
+		}
+		
+		for(int indice = 1; indice < n; indice++)
+		{
+			tmpI = i;
+			tmpJ = j;
+			tmpN = indice;
+			
+			if(j>= indice)
+			{
+				tmpJ = j - indice;
+				tmpN = n - indice;
+			}
+			
+			//System.out.println(m+" "+tmpN+" "+tmpI+" "+tmpJ);
+			if(!tabChecked[Simulate4D.convert(m, tmpN, tmpI, tmpJ, m_initial, n_initial, m_initial)])
+			{
+				System.out.println(new Symmetry(m,tmpN,tmpI,tmpJ));
+				res = f_dp_naif(m, tmpN, tmpI, tmpJ,false);
+				tabRes[Simulate4D.convert(m, tmpN, tmpI, tmpJ, m_initial, n_initial, m_initial)]=res;
+				tabChecked[Simulate4D.convert(m, tmpN, tmpI, tmpJ, m_initial, n_initial, m_initial)]=true;
+			}
+			else
+				res = tabRes[Simulate4D.convert(m, tmpN, tmpI, tmpJ, m_initial, n_initial, m_initial)];
+			
+			
+			if (res<1)
+			{
+				hasNeg = true;
+				maxNegativ = Math.max(maxNegativ, res);
+			}
+			else
+				maxPositiv = Math.max(maxNegativ, res);
+			
+		}
+		
+		
+		
+		if (hasNeg)
+			res = maxNegativ-1;
+		else
+			res = maxPositiv+1;
+		
+		return -res;
+		
 	}
+	
 	
 	public int f_dp_naif(int m,int n,int i,int j)
 	{
+		int sizetab = m+m*n+m*n*m+m*n*m*n;
+		this.m_initial=m;
+		this.n_initial=n;
+		
+		tabChecked = new boolean[sizetab];
+		tabRes = new int[sizetab];
+		
+		for(int a=0;a<sizetab;a++)
+		{
+			tabRes[a]=0;
+			tabChecked[a]=false;
+		}
+		tabChecked[Simulate4D.convert(1, 1, 0, 0, this.m_initial, this.n_initial, this.m_initial)]=true;
+		
 		return f_dp_naif(m,n,i,j,true);
 	}
 	
+	public int f_dp_symmetry(int m,int n,int i,int j ,boolean resetCounter)
+	{
+		if(resetCounter)
+			this.compteurAppel=0;
+		this.compteurAppel++;
+		
+		System.out.println(m+" "+n+" "+i+" "+j);
+		
+		if(m==1 && n==1)
+			return 0;
+		
+		int tmpI,tmpJ,res,tmpM,tmpN = -1;
+		int maxNegativ = Integer.MIN_VALUE;
+		boolean hasNeg = false;
+		int maxPositiv = 0;
+		
+		res = 0;
+		for(int indice = 1; indice < m; indice++ )
+		{		
+			tmpI = i;
+			tmpJ = j;
+			tmpM = indice;
+			
+			if(i>= indice)
+			{
+				tmpI = i - indice;
+				tmpM = m-indice;
+			}
+			
+			Symmetry s = new Symmetry(tmpM,n,tmpI,tmpJ);
+			s = s.normalizedSymmetry();
+			if(tabRes[Simulate4D.convert(s.m, s.n, s.i, s.j, m_initial, n_initial, m_initial)]==0)
+			{
+				res = f_dp_symmetry(s.m, s.n, s.i, s.j,false);
+				tabRes[Simulate4D.convert(s.m, s.n, s.i, s.j, m_initial, n_initial, m_initial)]=res;
+			}
+			else
+				res = tabRes[Simulate4D.convert(s.m, s.n, s.i, s.j, m_initial, n_initial, m_initial)];
+			
+			if (res<1)
+			{
+				hasNeg = true;
+				maxNegativ = Math.max(maxNegativ, res);
+			}
+			else
+				maxPositiv = Math.max(maxNegativ, res);
+			
+		}
+		
+		for(int indice = 1; indice < n; indice++)
+		{
+			tmpI = i;
+			tmpJ = j;
+			tmpN = indice;
+			
+			if(j>= indice)
+			{
+				tmpJ = j - indice;
+				tmpN = n - indice;
+			}
+			
+			Symmetry s = new Symmetry(m,tmpN,tmpI,tmpJ);
+			s = s.normalizedSymmetry();
+			if(tabRes[Simulate4D.convert(s.m, s.n, s.i, s.j, m_initial, n_initial, m_initial)]==0)
+			{
+				res = f_dp_symmetry(s.m, s.n, s.i, s.j,false);
+				tabRes[Simulate4D.convert(s.m, s.n, s.i, s.j, m_initial, n_initial, m_initial)]=res;
+			}
+			else
+				res = tabRes[Simulate4D.convert(s.m, s.n, s.i, s.j, m_initial, n_initial, m_initial)];
+			
+			
+			if (res<1)
+			{
+				hasNeg = true;
+				maxNegativ = Math.max(maxNegativ, res);
+			}
+			else
+				maxPositiv = Math.max(maxNegativ, res);
+			
+		}
+		
+		
+		
+		if (hasNeg)
+			res = maxNegativ-1;
+		else
+			res = maxPositiv+1;
+		
+		return -res;
+		
+	}
+	
+	
+	public int f_dp_symmetry(int m,int n,int i,int j)
+	{
+		int sizetab = m+m*n+m*n*m+m*n*m*n;
+		this.m_initial=m;
+		this.n_initial=n;
+		
+		
+		tabRes = new int[sizetab];
+		
+		for(int a=0;a<sizetab;a++)
+			tabRes[a]=0;
+		
+		return f_dp_symmetry(m,n,i,j,true);
+	}
 }
