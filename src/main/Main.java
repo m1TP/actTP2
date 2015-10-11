c93165824ffdfd5e003587e4bb21a66edb79be18
Julien Rouse <julien.rouse@gmail.com>
2015-10-12 01:10:08 +0200

    symmetry

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import util.Simulate4D;
+import util.Symmetry;
 import implementation.Exo;
 
 public class Main {
@@ -15,25 +16,63 @@ public class Main {
 
 		Exo exo = new Exo();
 		
-		int m = 3;
-		int n = 3;
+		/*
+		int m = 5;
+		int n = 5;
+		int i = 0;
+		int j = 0;
+		//*/
+		int res = 0;
 		
-		int res = exo.f_naif(m, n, 2, 2);
 		
-		res = exo.f_naif(10, 7, 7, 3);
+		//res = exo.f_naif(m, n, i, j);
+		//System.out.println("res: "+ res+" en "+exo.compteurAppel+" appels récursifs");
+		
+		/*
+		for(int m=2;m<10;m++)
+			for(int n=2;n<10;n++)
+				for(int i=0;i<m;i++)
+					for(int j=0;j<n;j++)
+					{
+						int res1 = exo.f_dp_naif(m, n, i, j);
+						
+						int res2 = exo.f_dp_symmetry(m, n, i, j);
+						
+						if(res1!=res2)
+							System.out.println(new Symmetry(m,n,i,j));
+					}
+		//*/
+		/*
+		res = exo.f_dp_naif(m, n, i, j);
 		System.out.println("res: "+ res+" en "+exo.compteurAppel+" appels récursifs");
 		
-		res = exo.f_naif(10, 7, 5, 3);
+		res = exo.f_dp_symmetry(m, n, i, j);
 		System.out.println("res: "+ res+" en "+exo.compteurAppel+" appels récursifs");
 		
-		for(int i=0;i<m;i++)
-			for(int j=0;j<n;j++)
-				;//System.out.println("res: "+exo.f_naif(m, n, i, j)+" en "+exo.compteurAppel+" appels récursifs") ;
+		//*/
 		
+		res = exo.f_dp_naif(2, 5, 0, 1);
+		System.out.println("res: "+ res+" en "+exo.compteurAppel+" appels récursifs");
 		
-		System.out.println(Simulate4D.convert(100, 90, 45, 6, 1300, 1300, 1300));
+		res = exo.f_dp_symmetry(2, 5, 0, 1);
+		System.out.println("res: "+ res+" en "+exo.compteurAppel+" appels récursifs");
 		
+		/*
+		for(int i1 = 0; i1<127; i1++)
+		{
+			for( int j1 =0; j1<127; j1++)
+			{
+				if (exo.f_dp_naif(100, 100, i1, j1)==127)
+					System.out.println(i+" "+j);
+			}
+		}
+		//*/
 		
+		/*
+		Symmetry s = new Symmetry(4, 5, 3, 3);
+		s = s.normalizedSymmetry();
+		System.out.println(s);
+		//*/
 	}
 
 }
---
+++ src/util/Symmetry.java
@@ -0,0 +1,116 @@
+package util;
+
+public class Symmetry {
+
+	public int m,n,i,j;
+	
+	public Symmetry(int m,int n, int i, int j)
+	{
+		this.m=m;
+		this.n=n;
+		this.i=i;
+		this.j=j;
+
+	}
+	
+	/**
+	 * produce every symmetry for a quadruplet given, an return the one that is normalized.
+	 * ie we keep m and n such as m>n if m!=n ans such as i be the minimum, and then j minimum.
+	 * 
+	 */
+	public Symmetry normalizedSymmetry()
+	{
+		int mm = this.m;
+		int nn = this.n;
+		int ii = this.i;
+		int jj = this.j;
+		
+		if(m<n)
+		{
+			mm = this.n;
+			nn = this.m;
+			ii = this.j;
+			jj = this.i;
+		}
+		
+		Symmetry res = new Symmetry(mm,nn,ii,jj);
+		
+		for(int a=0;a<m;a++)
+			for(int b=0;b<n;b++)
+			{
+				if(res.isSymmetry(res.m, res.n, a, b))
+				{
+					if (a<res.i)
+					{
+						res.i = a;
+						res.j = b;
+					}
+					else if (a==res.i && b<res.j)
+						res.j=b;
+				}
+			}
+		
+		return res;
+		
+	}
+
+	public boolean isSymmetry(int m, int n, int i, int j)
+	{
+		
+		if(this.m == m && this.n == n && this.i == i && this.j == j)
+			return true;
+		
+		int mm = m;
+		int nn = n;
+		int ii = i;
+		int jj = j;
+		if(this.m == n && this.n == m && m!=n)
+		{
+			mm = n;
+			nn = m;
+			ii = j;
+			jj = i;
+		}
+		
+		if(this.m == mm && this.n == nn && this.i == ii && this.j == jj)
+			return true;
+		
+		//System.out.println(verticalSymmetry(mm, nn, ii, jj) + " " + horizontalSymmetry(mm, nn, ii, jj) +" "+ horizontalAndVerticalSymmetry(mm, nn, ii, jj));
+		return verticalSymmetry(mm, nn, ii, jj) || horizontalSymmetry(mm, nn, ii, jj) || horizontalAndVerticalSymmetry(mm, nn, ii, jj);
+		
+	}
+	
+	public boolean verticalSymmetry(int m,int n,int i,int j)
+	{
+		
+		if(!(this.m==m && this.n==n && this.j == j)) //on a le mm tableau
+			return false;
+		
+		if(this.m-1-i==this.i) 
+			return true;
+		return false;
+	}
+	
+	public boolean horizontalSymmetry(int m,int n,int i,int j)
+	{
+		if(!(this.m==m && this.n==n && this.i == i)) //on a le mm tableau
+			return false;
+		
+		if(this.n-1-j==this.j) 
+			return true;
+		return false;
+	}
+	
+	public boolean horizontalAndVerticalSymmetry(int m,int n,int i,int j)
+	{
+		if(!(this.m==m && this.n==n))
+			return false;
+	
+		return verticalSymmetry(m,n,i,this.j) && horizontalSymmetry(m,n,this.i,j);
+	}
+	
+	public String toString()
+	{
+		return this.m+" "+this.n+" "+this.i+" "+this.j;
+	}
+}
