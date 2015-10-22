98451e5e1e128ac62fdfb4708deff649d21c73b9
ChristopherDeshaies <llyodoisdu59@hotmail.fr>
2015-10-22 23:39:34 +0200

    classe Gamev2
    Le jeu se déroule désormais avec le joueur 1 qui est devenu une IA et le
    joueur 2 un joueur réel

--- src/main/Game.java
+++ src/main/Game.java
@@ -2,6 +2,8 @@ package main;
 
 import java.util.Random;
 import java.util.Scanner;
+
+import util.Simulate4D;
 import implementation.Exo_v2;
 
 public class Game {
@@ -45,7 +47,8 @@ public class Game {
 		}
 		System.out.println("Initialisation du plateau terminé.");
 		
-		
+		int m_init=m;
+		int n_init=n;
 		
 		
 		//Placement du carre de la mort
@@ -66,8 +69,8 @@ public class Game {
 		
 		System.out.println("C'est l'heure du duel !\n");
 		int res;
-		boolean joueur=true;
-		String joueur1 = "Joueur 1";
+		boolean joueur=false;
+		String joueur1 = "Joueur 1 (Ordinateur)";
 		String joueur2 = "Joueur 2";
 		String joueurActuel = "";
 		
@@ -78,7 +81,7 @@ public class Game {
 			System.out.println("Représentation du plateau de jeu");
 			representationPlateau(m,n,i,j);
 			
-			if(joueur)
+			if(!joueur)
 				joueurActuel=joueur1;
 			else
 				joueurActuel=joueur2;
@@ -97,79 +100,173 @@ public class Game {
 			
 			
 			
-			
-			System.out.println("Coupe a faire en longueur ou largueur (long/larg) ?");
-			String tmpLine = "";
-			int tmpNum = 0;
-			scan = new Scanner(System.in);
-			boolean longueur = false;
-			boolean largueur = false;
-			
-			while( !longueur && !largueur )
-				if(tmpLine.equals("long")){
-					longueur=true;
-				}else if(tmpLine.equals("larg"))
-					largueur=true;
-				else
-					tmpLine=scan.nextLine();
+			if(joueur){ //Tour Joueur
+				System.out.println("Coupe a faire en longueur ou largueur (long/larg) ?");
+				String tmpLine = "";
+				int tmpNum = 0;
+				scan = new Scanner(System.in);
+				boolean longueur = false;
+				boolean largueur = false;
 				
-			
-			if(longueur){
-				System.out.println("Couper à partir de la colonne (inclu) :");
-				while(tmpNum<=0 || tmpNum>=m){
-					if(scan.hasNextInt() && (tmpNum=scan.nextInt()) > 0){
-						if(tmpNum < m)
-							System.out.println("On coupe ...");
-						else{
+				while( !longueur && !largueur )
+					if(tmpLine.equals("long")){
+						longueur=true;
+					}else if(tmpLine.equals("larg"))
+						largueur=true;
+					else
+						tmpLine=scan.nextLine();
+					
+				
+				if(longueur){
+					System.out.println("Couper à partir de la colonne (inclu) :");
+					while(tmpNum<=0 || tmpNum>=m){
+						if(scan.hasNextInt() && (tmpNum=scan.nextInt()) > 0){
+							if(tmpNum < m)
+								System.out.println("On coupe ...");
+							else{
+								System.out.println("Indiquez un entier supérieur à 0 ... et inférieur à la longueur du tableau actuel "+m);
+							}
+						}else{
 							System.out.println("Indiquez un entier supérieur à 0 ... et inférieur à la longueur du tableau actuel "+m);
 						}
-					}else{
-						System.out.println("Indiquez un entier supérieur à 0 ... et inférieur à la longueur du tableau actuel "+m);
 					}
-				}
-			}else{
-				System.out.println("Couper à partir de la ligne (inclu) :");
-				while(tmpNum<=0 || tmpNum>=n){
-					if(scan.hasNextInt() && (tmpNum=scan.nextInt()) > 0){
-						if(tmpNum < n)
-							System.out.println("On coupe ...");
-						else{
+				}else{
+					System.out.println("Couper à partir de la ligne (inclu) :");
+					while(tmpNum<=0 || tmpNum>=n){
+						if(scan.hasNextInt() && (tmpNum=scan.nextInt()) > 0){
+							if(tmpNum < n)
+								System.out.println("On coupe ...");
+							else{
+								System.out.println("Indiquez un entier supérieur à 0 ... et inférieur à la largueur du tableau actuel "+n);
+							}
+						}else{
 							System.out.println("Indiquez un entier supérieur à 0 ... et inférieur à la largueur du tableau actuel "+n);
 						}
-					}else{
-						System.out.println("Indiquez un entier supérieur à 0 ... et inférieur à la largueur du tableau actuel "+n);
 					}
 				}
-			}
-			
-			
-			if(longueur){
-				if(tmpNum<=i){//Coupe a gauche
-					System.out.println("Coupe a gauche");
-					m=m-tmpNum;
-					i=i-tmpNum;
-				}else{//Coupe a droite
-					System.out.println("Coupe a droite");
-					m=tmpNum;
+				
+				//Coupe
+				if(longueur){
+					if(tmpNum<=i){//Coupe a gauche
+						m=m-tmpNum;
+						i=i-tmpNum;
+					}else{//Coupe a droite
+						m=tmpNum;
+					}
+				}else{
+					if(tmpNum<=j){//Coupe en haut
+						n=n-tmpNum;
+						j=j-tmpNum;
+					}else{//Coupe en bas
+						n=tmpNum;
+					}				
 				}
-			}else{
-				if(tmpNum<=j){//Coupe en haut
-					n=n-tmpNum;
-					j=j-tmpNum;
-				}else{//Coupe en bas
-					n=tmpNum;
-				}				
+			
+			}else{//Tour ordinateur
+				System.out.println("L'ordinateur calcul la meilleure coupe à faire ...");
+				
+				/*for(int z=0;z<exo2.tabRes.length;z++){
+					System.out.println(exo2.tabRes.length+ " " +z+" "+exo2.tabRes[z]);
+				}*/
+				
+				
+				//Recherche de la coupe a faire
+				boolean hasNeg = false;
+				int tmpM = 0;
+				int tmpN = 0;
+				int tmpI = 0;
+				int tmpJ = 0;
+				int maxPositiv = 0;
+				int maxNegativ = 0;
+				
+				if(res>0){
+					hasNeg=true;
+				}
+				
+				for(int k=0;k<m;k++)
+					for(int l=0;l<n;l++)
+						for(int p=0;p<m;p++)
+							for(int q=0;q<n;q++){
+								//System.out.println(" Pour maxNegativ = "+maxNegativ+" on a tabRes : "+exo2.tabRes[Simulate4D.convert(k, l, p, q, m, n, m)]+" avec "+(Simulate4D.convert(k, l, p, q, m, n, m)));
+								if(hasNeg){
+									//tmpmaxNegativ = Math.min(maxNegativ, exo2.tabRes[Simulate4D.convert(k, l, p, q, m, n, m)]);
+									if(Simulate4D.convert(k, l, p, q, m_init, n_init, m_init)<exo2.tabRes.length)
+										if(exo2.tabRes[Simulate4D.convert(k, l, p, q, m_init, n_init, m_init)]<maxNegativ){
+											//System.out.println("Trouvé ! en neg"+" m ="+k+" n ="+l+" i ="+p+" j ="+q);
+											if(k>0 || l>0){
+												//System.out.println("Trouvé ! en neg"+" m ="+k+" n ="+l+" i ="+p+" j ="+q);
+												maxNegativ=exo2.tabRes[Simulate4D.convert(k, l, p, q, m_init, n_init, m_init)];
+												tmpM=k;
+												tmpN=l;
+												tmpI=p;
+												tmpJ=q;
+											}
+											
+										}
+								
+								}else{
+									if(Simulate4D.convert(k, l, p, q, m_init, n_init, m_init)<exo2.tabRes.length)
+										if(exo2.tabRes[Simulate4D.convert(k, l, p, q, m_init, n_init, m_init)]>maxPositiv){
+											//System.out.println("Trouvé ! en neg"+" m ="+k+" n ="+l+" i ="+p+" j ="+q);
+											if(k>0 || l>0){
+												//System.out.println("Trouvé ! en neg"+" m ="+k+" n ="+l+" i ="+p+" j ="+q);
+												maxPositiv=exo2.tabRes[Simulate4D.convert(k, l, p, q, m_init, n_init, m_init)];
+												tmpM=k;
+												tmpN=l;
+												tmpI=p;
+												tmpJ=q;
+											}
+											
+										}
+								}
+								
+							}
+						
+
+				
+				//Coupe
+				if(tmpM>0){
+					if(tmpI!=i){//Coupe a gauche
+						System.out.println('g');
+						m=tmpM;
+						i=tmpI;
+					}else{//Coupe a droite
+						System.out.println('d');
+						m=tmpM;
+						i=tmpI;
+					}
+				}else if(tmpN>0){
+					if(tmpJ!=j){//Coupe en haut
+						System.out.println('h');
+						n=tmpN;
+						j=tmpJ;
+					}else{//Coupe en bas
+						System.out.println('b');
+						n=tmpN;
+						j=tmpJ;
+					}				
+				}else{
+					m=1;n=1;i=0;j=0;
+				}
+				
 			}
+			
+			
 			
 			System.out.println("Coupe terminé ... à l'adversaire ...");
 			joueur=!joueur;
 			
 		}
 		
+		
+		
 		System.out.println("\n Félicitation le "+joueurActuel+" a gagné !");
 		
 		
 	}
+
+	
+	
 	
 	public static void representationPlateau(int m, int n, int i, int j){
 		for(int k=0;k<n;k++){
